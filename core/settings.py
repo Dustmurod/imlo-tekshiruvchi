b288de22e3dcda2940931af5c6bab919cc2d84cb
Dustmurod <92831907+Dustmurod@users.noreply.github.com>
2026-04-08 19:49:41 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2!q1+&ogt*kf5k1t67d2jgx=8b%b%!goc1g&-+8hj1w#p)3owk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['imlo-tekshiruvchi-4.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['bmi-imlo-tekshiruvchi.onrender.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
